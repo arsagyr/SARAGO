5c408adb8ea6bed68f24d90592df542cdeb3f5bf
Arsenius Chalenistkhai <arsagyr@debian.arsagyr.ru>
2025-04-11 10:14:12 +0700

    Arithmetic functions of DRV is added

--- drv/DRV.go
+++ drv/DRV.go
@@ -115,6 +115,86 @@ func (drv1 DRV) SumDRV(drv2 DRV) DRV {
 	return drv
 }
 
-// func SubtractionDRV(drv1 DRV, drv2 DRV) DRV {}
-// func ProductDRV(drv1 DRV, drv2 DRV) DRV {}
-// func RatioDRV(drv1 DRV, drv2 DRV) DRV {}
+func SubtractionDRV(drv1 DRV, drv2 DRV) DRV {
+	var ar1 []float32 
+	var ar2 []fraction.Fraction
+	var k int = 0
+	for i := 0; i < drv1.size; i++ {
+		for j := 0; j < drv2.size; j++ {
+			ar1 = append( ar1, drv1.values[i] - drv2.values[j])
+			ar2 = append( ar2, drv1.probabilities[i].MultiplyByFraction(drv2.probabilities[j]))
+			k = k + 1
+		}
+	}
+	for i := 0; i < k; i++ {
+		for j := i + 1; j < k; j++ {
+			if ar1[i] == ar1 [j] {
+				ar2[i] = ar2[i].SumByFraction(ar2[j])
+				ar2[j].SetNumerator(0)
+			}
+		}
+	}
+	var drv = DRV{
+		size:          k,
+		probabilities: ar2,
+		values:        ar1,
+	}
+	drv = CleanDRV(drv)
+	return drv
+}
+
+func ProductDRV(drv1 DRV, drv2 DRV) DRV {
+	var ar1 []float32 
+	var ar2 []fraction.Fraction
+	var k int = 0
+	for i := 0; i < drv1.size; i++ {
+		for j := 0; j < drv2.size; j++ {
+			ar1 = append( ar1, drv1.values[i] * drv2.values[j])
+			ar2 = append( ar2, drv1.probabilities[i].MultiplyByFraction(drv2.probabilities[j]))
+			k = k + 1
+		}
+	}
+	for i := 0; i < k; i++ {
+		for j := i + 1; j < k; j++ {
+			if ar1[i] == ar1 [j] {
+				ar2[i] = ar2[i].SumByFraction(ar2[j])
+				ar2[j].SetNumerator(0)
+			}
+		}
+	}
+	var drv = DRV{
+		size:          k,
+		probabilities: ar2,
+		values:        ar1,
+	}
+	drv = CleanDRV(drv)
+	return drv
+}
+
+func RatioDRV(drv1 DRV, drv2 DRV) DRV {
+	var ar1 []float32 
+	var ar2 []fraction.Fraction
+	var k int = 0
+	for i := 0; i < drv1.size; i++ {
+		for j := 0; j < drv2.size; j++ {
+			ar1 = append( ar1, drv1.values[i] / drv2.values[j])
+			ar2 = append( ar2, drv1.probabilities[i].MultiplyByFraction(drv2.probabilities[j]))
+			k = k + 1
+		}
+	}
+	for i := 0; i < k; i++ {
+		for j := i + 1; j < k; j++ {
+			if ar1[i] == ar1 [j] {
+				ar2[i] = ar2[i].SumByFraction(ar2[j])
+				ar2[j].SetNumerator(0)
+			}
+		}
+	}
+	var drv = DRV{
+		size:          k,
+		probabilities: ar2,
+		values:        ar1,
+	}
+	drv = CleanDRV(drv)
+	return drv
+}
